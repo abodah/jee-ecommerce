5a690907d5e97977685e2a51a1ef61efef698609
DECANET LUC ALFRED <alfred@decanet.fr>
2022-07-14 07:45:48 +0100

    Entities editions... incomming modifications on journey

---
+++ catalogue/src/main/java/org/jee/ecommerce/entity/Category.java
@@ -0,0 +1,12 @@
+package org.jee.ecommerce.entity;
+
+import java.util.Date;
+
+public class Category {
+	private long id;
+	private String name;
+	private String description;
+	private boolean active;
+	private Date createAt;
+	private Date updateAt;
+}
